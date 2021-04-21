34cdd7c6434611d84f758d9194c4e23f1dceb750
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-04-20 19:47:15 -0700

    connecting to database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
 
 const PORT = process.env.PORT || 5000;
 
