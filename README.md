408db36e11c8f5ad0364580c2465e8c2bb8fa71f
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-04-20 17:51:27 -0700

    starting over

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # comic_collector_app
-a MERN Stack app that allows users to collect and store comics in a digital database 
+a MERN Stack app that allows users to collect and store comics in a digital database. 
--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-// const path = require("path");
-
-const app = express();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT${PORT}`));
